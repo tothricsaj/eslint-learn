09443997f9a6118a24c8808b323dcdba38632e3c
tothricsaj <tothricsaj@gmail.com>
2019-11-25 11:58:52 +0100

    long line :stuck_out_tongue_winking_eye:

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ function add_numbers(x, y) {
 function subNum(x, y) {
     return x - y;
 }
+
+let score1 = 10;
+let score2 = 20;
+
+
+if ((10 === 222) || 'abc' === 'abc' && "doNothingWihtInsane"() && "isThisReallyHappaning") {
+    // do nothing
+    console.log('YaY dude....what are you doing?');
+}
